8f94db64e18006dca2e4e2a2a1c8e57f28ffd5dc
Alexander Girke <alexander.girke@mercateo.com>
2019-07-04 16:48:46 +0200

    update run.sh + add first visualization

--- run.sh
+++ run.sh
@@ -9,5 +9,28 @@ then
 fi
 
 docker-compose up -d --build
+# waiting for ELK to be up and running
+until [ -n "$(curl http://localhost:9200/_cat/indices)" ]; do
+    sleep 10;
+done;
 
-sudo sysdig -j -pc container.name!=host > logs/sysdig/syscalls.log
+
+# -------- CONFIGURING ELASTICSEARCH --------
+curl -X PUT http://localhost:9200/filebeat-default
+curl -X PUT http://localhost:9200/filebeat-default/_mappings -H "Content-Type:application/json" -d@elasticsearch-default-filebeat-mapping.json
+
+# -------- CONFIGURING KIBANA --------
+# define the kibana index pattern
+KIBANA_INDEX_PATTERN_ID=$(curl http://localhost:5601/api/saved_objects/index-pattern \
+	-H "Content-Type:application/json" \
+	-H "kbn-version:7.2.0" \
+	-d '{"attributes":{"title":"filebeat*","timeFieldName":"@timestamp"}}' \
+	| jq '.id')
+# set this index pattern as default
+curl http://localhost:5601/api/kibana/settings -H "Content-Type:application/json" -H "kbn-version:7.2.0" -d '{"changes":{"defaultIndex":'${KIBANA_INDEX_PATTERN_ID}'}}'
+
+# add visualizations
+curl http://localhost:5601/api/saved_objects/_import -H kbn-version:7.2.0 --form file=@evt-type-pie-chart.ndjson
+
+# TODO re-enable
+# sudo sysdig -j -pc container.name!=host > logs/sysdig/syscalls.log
