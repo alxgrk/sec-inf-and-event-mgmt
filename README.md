25868eb510c7c0e972fa48e93b28c2762bd1e720
Alexander Girke <alexander.girke@mercateo.com>
2019-08-01 22:09:10 +0200

    minor restructuring

--- README.md
+++ README.md
@@ -2,30 +2,16 @@
 This is a universitary project playing aroung with the ELK stack and Sysdig.
 
 ## Start
-To run the whole system at once, simply call
+To run the whole system at once, simply call this script with the container name to track, e.g.
 ```bash
- $ ./run.sh
+ $ ./run.sh dvwa-sql-app_victim_1
 ```
-If you want sysdig to track syscalls, simply add
+If you don't want sysdig to track syscalls, simply add
 ```bash
- $ ./run.sh --track
+ $ ./run.sh --notrack
 ```
 This will fix potential problems (see below), bring up all containers & watch their syscalls with sysdig.
 
 ## References
 The setup is heavily based on https://elk-docker.readthedocs.io/
 
-## Frequently encountered issues
-_mostly taken from the above reference_
-
-### Max Map Count
-Problem:
-```
-max virtual memory areas vm.max_map_count [65530] likely too low, increase to at least [262144]
-```
-Solution: 
-```
-sudo sysctl -w vm.max_map_count=262144
-```
-## TODO
- - refresh index mapping after a the first document was sent (warning can be found in kibana)
--- run.sh
+++ run.sh
@@ -1,69 +1,85 @@
 #!/bin/bash
 
-# set -x
+set -x
 
-TRACKED_CONTAINER=dvwa-sql-app_victim_1 #${1:-pratikum-sec-inf-and-event-mgmt_dummy-app_1}
+# default values
+DUMMY_APP=pratikum-sec-inf-and-event-mgmt_dummy-app_1
+TRACKED_CONTAINER=${DUMMY_APP}
+TRACK_SYSCALLS=true
 
-if [ -z "$(which jq)" ]
-then
-    echo "please install 'jq' first (e.g. 'sudo apt install jq')"
-    exit 1
-fi
+for PARAM in "$@"
+do
 
-if [ -n "$1" ] 
-then
-    if [ "--track" != "$1" ]
+    if [[ "--notrack" = "${PARAM}" ]]
     then
-        echo "specify '--track' to enable sysdig syscall tracking"
-        exit 1
+        TRACK_SYSCALLS=false
     else
-        TRACK_SYSCALLS=true
+        TRACKED_CONTAINER=${PARAM}
     fi
-else
-    TRACK_SYSCALLS=false
-fi
 
-# fix potential error when running elk
-if [ $(sysctl vm.max_map_count | awk '{print $3}') -lt 262144 ] 
-then 
-	sudo sysctl -w vm.max_map_count=262144
-fi
+done
 
-docker-compose up -d --build
-# waiting for ELK to be up and running
-until [ -n "$(curl --silent -w "\n" http://localhost:9200/_cat/indices)" -a "$(curl --silent -w "\n" http://localhost:5601/api/kibana/settings -H "kbn-version:7.2.0")" != "Kibana server is not ready yet" ]; do
-    echo "waiting for ELK to come up..."
-    sleep 5;
-done;
+main() {
+    docker-compose up -d --build
+    # waiting for ELK to be up and running
+    until [[ -n "$(curl --silent -w "\n" http://localhost:9200/_cat/indices)" \
+     && "$(curl --silent -w "\n" http://localhost:5601/api/kibana/settings -H "kbn-version:7.2.0")" != "Kibana server is not ready yet" ]];
+    do
+        echo "waiting for ELK to come up..."
+        sleep 5;
+    done;
 
 
-# -------- CONFIGURING ELASTICSEARCH --------
-echo "create default ES index: "
-curl --silent -w "\n" -X PUT http://localhost:9200/filebeat-default
-echo "define index mapping: "
-curl --silent -w "\n" -X PUT http://localhost:9200/filebeat-default/_mappings -H "Content-Type:application/json" -d@elasticsearch-default-filebeat-mapping.json
+    # -------- CONFIGURING ELASTICSEARCH --------
+    echo "create default ES index: "
+    curl --silent -w "\n" -X PUT http://localhost:9200/filebeat-default
+    echo "define index mapping: "
+    curl --silent -w "\n" -X PUT http://localhost:9200/filebeat-default/_mappings -H "Content-Type:application/json" -d@elasticsearch-default-filebeat-mapping.json
 
-# -------- CONFIGURING KIBANA --------
-KIBANA_INDEX_PATTERN_ID=8098fa00-9e67-11e9-a82b-7bda944bfa90
-echo "define the kibana index pattern with id $KIBANA_INDEX_PATTERN_ID: "
-curl --silent -w "\n" http://localhost:5601/api/saved_objects/index-pattern/8098fa00-9e67-11e9-a82b-7bda944bfa90?overwrite=true \
-	-H "Content-Type:application/json" \
-	-H "kbn-version:7.2.0" \
-	-d '{"attributes":{"title":"filebeat*","timeFieldName":"@timestamp"}}'
-echo "set this index pattern as default: "
-curl --silent -w "\n" http://localhost:5601/api/kibana/settings -H "Content-Type:application/json" -H "kbn-version:7.2.0" -d '{"changes":{"defaultIndex":'${KIBANA_INDEX_PATTERN_ID}'}}'
+    # -------- CONFIGURING KIBANA --------
+    KIBANA_INDEX_PATTERN_ID=8098fa00-9e67-11e9-a82b-7bda944bfa90
+    echo "define the kibana index pattern with id $KIBANA_INDEX_PATTERN_ID: "
+    curl --silent -w "\n" http://localhost:5601/api/saved_objects/index-pattern/8098fa00-9e67-11e9-a82b-7bda944bfa90?overwrite=true \
+        -H "Content-Type:application/json" \
+        -H "kbn-version:7.2.0" \
+        -d '{"attributes":{"title":"filebeat*","timeFieldName":"@timestamp"}}'
+    echo "set this index pattern as default: "
+    curl --silent -w "\n" http://localhost:5601/api/kibana/settings -H "Content-Type:application/json" -H "kbn-version:7.2.0" -d '{"changes":{"defaultIndex":'${KIBANA_INDEX_PATTERN_ID}'}}'
 
-echo "add visualizations & dashboard: "
-curl --silent -w "\n" http://localhost:5601/api/saved_objects/_import?overwrite=true -H kbn-version:7.2.0 --form file=@dashboard_and_visualizations.ndjson
+    echo "add visualizations & dashboard: "
+    curl --silent -w "\n" http://localhost:5601/api/saved_objects/_import?overwrite=true -H kbn-version:7.2.0 --form file=@dashboard_and_visualizations.ndjson
 
-if [ $TRACK_SYSCALLS == true ]
-then
-    echo "now running sysdig to log syscalls";
+    if [[ ${TRACK_SYSCALLS} = true ]]
+    then
+        echo "now running sysdig to log syscalls";
 
-    # query the dummy app
-    trap "kill 0" EXIT
-    while true; do curl --silent localhost:8080 > /dev/null; sleep 0.5; done &
+        if [[ ${TRACKED_CONTAINER} = ${DUMMY_APP} ]]
+        then
+            echo "query the dummy app every 500ms"
+            trap "kill 0" EXIT
+            while true; do curl --silent localhost:8080 > /dev/null; sleep 0.5; done &
+        fi
 
-    sudo sysdig -j -s 8000 -pc container.name=${TRACKED_CONTAINER} > logs/sysdig/syscalls.log 
-fi
+        sudo sysdig -j -s 8000 -pc container.name=${TRACKED_CONTAINER} > logs/sysdig/syscalls.log
+    fi
+}
 
+sanity_checks() {
+
+    # check if 'jq' is installed
+    if [ -z "$(which jq)" ]
+    then
+        echo "please install 'jq' first (e.g. 'sudo apt install jq')"
+        exit 1
+    fi
+
+    # fix potential error when running elk
+    if [ $(sysctl vm.max_map_count | awk '{print $3}') -lt 262144 ] 
+    then 
+        sudo sysctl -w vm.max_map_count=262144
+    fi
+
+}
+
+sanity_checks
+main
