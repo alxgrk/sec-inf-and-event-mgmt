3d94c171b22f53e28e935256a07018b32deaf3d2
Alexander Girke <alexander.girke@mercateo.com>
2019-08-03 01:19:03 +0200

    fix dvwa victim by using links in docker-compose.yml

--- dvwa-app/attacker/start-attacker.sh
+++ dvwa-app/attacker/start-attacker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 /home/attacker.py -ip "172.25.0.2" -u admin -p password
+python3 /home/attacker.py -ip "victim" -u admin -p password
--- dvwa-app/client/start-client.sh
+++ dvwa-app/client/start-client.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 /home/client.py -ip "172.25.0.2" -u admin -p password
+python3 /home/client.py -ip "victim" -u admin -p password
