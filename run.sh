c61f713142962cfafb079ab9f175323d4165c062
Alexander Girke <alexander.girke@mercateo.com>
2019-08-03 03:30:46 +0200

    fix minor bug in Kibana calls

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 # set -x
 
 # default values
+KIBANA_VERSION=7.2.0
 DUMMY_APP=pratikum-sec-inf-and-event-mgmt_dummy-app_1
 TRACKED_CONTAINER=${DUMMY_APP}
 TRACK_SYSCALLS=true
@@ -25,7 +26,7 @@ main() {
     docker-compose up -d --build
     # waiting for ELK to be up and running
     until [[ -n "$(curl --silent -w "\n" http://localhost:9200/_cat/indices)" \
-     && "$(curl --silent -w "\n" http://localhost:5601/api/kibana/settings -H "kbn-version:7.2.0")" != "Kibana server is not ready yet" ]];
+     && "$(curl --silent -w "\n" http://localhost:5601/api/kibana/settings -H "kbn-version:${KIBANA_VERSION}")" != "Kibana server is not ready yet" ]];
     do
         echo "waiting for ELK to come up..."
         sleep 5;
@@ -40,16 +41,16 @@ main() {
 
     # -------- CONFIGURING KIBANA --------
     KIBANA_INDEX_PATTERN_ID=8098fa00-9e67-11e9-a82b-7bda944bfa90
-    echo "define the kibana index pattern with id $KIBANA_INDEX_PATTERN_ID: "
-    curl --silent -w "\n" http://localhost:5601/api/saved_objects/index-pattern/8098fa00-9e67-11e9-a82b-7bda944bfa90?overwrite=true \
+    echo "define the kibana index pattern with id ${KIBANA_INDEX_PATTERN_ID}: "
+    curl --silent -w "\n" http://localhost:5601/api/saved_objects/index-pattern/${KIBANA_INDEX_PATTERN_ID}?overwrite=true \
         -H "Content-Type:application/json" \
-        -H "kbn-version:7.2.0" \
+        -H "kbn-version:${KIBANA_VERSION}" \
         -d '{"attributes":{"title":"filebeat*","timeFieldName":"@timestamp"}}'
     echo "set this index pattern as default: "
-    curl --silent -w "\n" http://localhost:5601/api/kibana/settings -H "Content-Type:application/json" -H "kbn-version:7.2.0" -d '{"changes":{"defaultIndex":'${KIBANA_INDEX_PATTERN_ID}'}}'
+    curl --silent -w "\n" http://localhost:5601/api/kibana/settings -H "Content-Type:application/json" -H "kbn-version:${KIBANA_VERSION}" -d '{"changes":{"defaultIndex":"'${KIBANA_INDEX_PATTERN_ID}'"}}'
 
     echo "add visualizations & dashboard: "
-    curl --silent -w "\n" http://localhost:5601/api/saved_objects/_import?overwrite=true -H kbn-version:7.2.0 --form file=@dashboard_and_visualizations.ndjson
+    curl --silent -w "\n" http://localhost:5601/api/saved_objects/_import?overwrite=true -H kbn-version:${KIBANA_VERSION} --form file=@dashboard_and_visualizations.ndjson
 
     if [[ ${TRACK_SYSCALLS} = true ]]
     then
@@ -67,7 +68,7 @@ main() {
         #  * in JSON format (-j)
         #  * replace port numbers by names were possible (-R, e.g. mysql port)
         #  * encode binary data as base64 (-b, e.g. evt.info.data field)
-        sudo sysdig -bRj -s 8000 -pc container.name=${TRACKED_CONTAINER} > logs/sysdig/syscalls.log
+        sudo sysdig -bRj -s 4000 -pc container.name=${TRACKED_CONTAINER} > logs/sysdig/syscalls.log
     fi
 }
 
